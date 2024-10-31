94eefad890bbc0124456a9809a31e9511626048b
Alex Thotse <30655517+alexthotse@users.noreply.github.com>
2024-10-31 01:56:30 +0000

    Killed it with a few hiccups

--- _03_04/ForLoops.java
+++ _03_04/ForLoops.java
@@ -4,11 +4,20 @@ public class ForLoops {
 
   public static void main(String[] args) {
     // Write a for loop that prints out the phrase "I love for loops" 5 times
+    int i;
+    for(i = 0; i < 5  ; i++){
+      System.out.println("I love for loops");
+    }
 
     // Write a for loop that prints out the numbers 1 to 10
+    for(i=1; i < 11; i++){
+      System.out.println(i);
+    }
 
     // Write a for loop that prints out the numbers 10 to 1
-    
+    for (i = 10; i > 0; i--) {
+      System.out.println(i);
+    }
 
   }
 
